be4ac42c8611be1466ef5c323820b4281eb4faea
MNT-Mantas <mnt@rootbox.ca>
2013-11-20 10:59:24 +0200

    Rename ZIP

--- envsetup.sh
+++ envsetup.sh
@@ -675,7 +675,7 @@ function eat()
 {
     if [ "$OUT" ] ; then
         MODVERSION=$(get_build_var CM_VERSION)
-        ZIPFILE=cm-$MODVERSION.zip
+        ZIPFILE=CyanFox-$MODVERSION.zip
         ZIPPATH=$OUT/$ZIPFILE
         if [ ! -f $ZIPPATH ] ; then
             echo "Nothing to eat"
